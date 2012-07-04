98fc5f1d91c0245b4a22341442aa02d08a8033aa
Dimitri Nüscheler <dimitri.nuescheler@sunrise.ch>
2012-07-04 23:02:18 +0200

    Halt it!

--- src/ircrpg2/Main.java
+++ src/ircrpg2/Main.java
@@ -118,7 +118,7 @@ public class Main {
         defaultWorld.getLibrary().persistPlayers();
         System.out.println(" done.");
         rpgBot.teardown("Shutdown signal");
-//	System.exit(0); //Immediately exit, as long as we have the lock.
+	System.halt(0); //Immediately exit, as long as we have the lock.
                 }
                 }
 		}
